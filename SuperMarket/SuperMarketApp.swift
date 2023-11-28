11442d8c32efadf5c7ffdeb446c1a30b370c9f62
codemanager255 <codemanager255@gmail.com>
2023-11-28 09:07:11 -0500

    Initial Commit

---
+++ SuperMarket/SuperMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperMarketApp.swift
+//  SuperMarket
+//
+//  Created by MAC on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
